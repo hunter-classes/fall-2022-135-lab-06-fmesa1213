1c76bcb8d16478285e892bd3dd3ae17b895758fe
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-14 03:56:01 -0400

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -2,4 +2,51 @@
 #include "doctest.h"
 #include "funcs.h"
 
-// add your tests here
+TEST_CASE("C"){
+ CHECK("Hello, World" == "Jevpq, Wyvnd!");
+   CHECK("good night" == "ioyh piqlv");
+   CHECK("good morning" == "ioyh oobrknq");
+//keyword == cake
+
+}
+       
+TEST_CASE("D"){
+
+ CHECK(
+ = Caesar =
+Enter shift    : 10
+Ciphertext     : qyyn xsqrd
+Decrypted      : good night
+
+= Vigenere =
+Enter keyword  : cake
+Ciphertext     : ioyh piqlv
+Decrypted      :good night
+
+ );
+   CHECK(
+   = Caesar =
+Enter shift    : 10
+Ciphertext     : qyyn wybxsxq
+Decrypted      : good morning
+
+
+= Vigenere =
+Enter keyword  : cake
+Ciphertext     : ioyh oobrknq
+Decrypted      :good morning
+
+
+   );
+   CHECK(= Caesar =
+Enter shift    : 10
+Ciphertext     : Rovvy, Gybvn!
+Decrypted      : Hello, World!
+
+= Vigenere =
+Enter keyword  : cake
+Ciphertext     : Jevpq, Wyvnd!
+Decrypted      : Hello, World!
+);
+}
+       
